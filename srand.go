e082d679974e0068a0a5cf33a2684028f57f777f
Aaron Hurt <ahurt@ena.com>
2019-04-04 15:59:30 -0500

    make it better

--- srand.go
+++ srand.go
@@ -1,57 +1,10 @@
 package secureRandom
 
 import (
-	crand "crypto/rand"
-	"encoding/base64"
+	"crypto/rand"
+	"encoding/base32"
 	"errors"
-	"fmt"
-	"math/big"
-	mrand "math/rand"
-	"strings"
 )
-
-// sanitized alphabet
-const saniBet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-
-// package random number generator
-var prng *mrand.Rand
-
-// init the package random number generator
-func init() {
-	var prime *big.Int // random secure prime
-	var err error      // error holder
-
-	// generate a 63 bit secure prime
-	if prime, err = crand.Prime(crand.Reader, 63); err != nil {
-		panic(fmt.Sprintf("secureRandom - failed to generate 63 bit prime seed: %s", err.Error()))
-	}
-
-	// initialize package random number generator
-	prng = mrand.New(mrand.NewSource(prime.Int64()))
-}
-
-// sanizize returns the passed string with all '-' and '_' characters replaced
-func sanitize(s string) string {
-	var alen = len(saniBet) // length of the sanitized alphabet
-	var split []string      // split input string
-
-	// split input string for looping
-	split = strings.Split(s, "")
-
-	// loop over split string and replace as needed
-	for idx, char := range split {
-		ridx := prng.Intn(alen)
-		if char == "-" {
-			split[idx] = saniBet[ridx : ridx+1]
-		}
-		if char == "_" {
-			split[idx] = saniBet[ridx : ridx+1]
-		}
-	}
-
-	// return the string - no error
-	return strings.Join(split, "")
-}
 
 // New builds a new secure salt of the given length
 func New(n int) (string, error) {
@@ -65,17 +18,17 @@ func New(n int) (string, error) {
 	}
 
 	// calculate random byte length needed to generate
-	// a base64 string of the requested length
-	randomSize = (6*n-5)/8 + 1
+	// a base32 string of the requested length
+	randomSize = (n * 5) / 8 + 1
 
 	// init random bytes
 	randomBytes = make([]byte, randomSize)
 
-	// seed random bytes
-	if _, err = crand.Read(randomBytes); err != nil {
+	// read random bytes
+	if _, err = rand.Read(randomBytes); err != nil {
 		return "", err
 	}
 
-	// return sanitized salt
-	return sanitize(base64.RawURLEncoding.EncodeToString(randomBytes)), nil
+	// return encoded salt
+	return base32.StdEncoding.WithPadding(base32.NoPadding).EncodeToString(randomBytes)[0:n], nil
 }
